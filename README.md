20a4395d846d29d3223ec0e9a02dacf17d7eda04
Jinkazuy <632931267@qq.com>
2019-09-12 17:08:11 +0800

    19.09.12

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 
 所用架构VUE-2 （vue-cli3）
 
-UI框架-自己写；
-
+UI框架-Bootstarp-3.3.7；
+https://segmentfault.com/a/1190000014509984
+1、安装
+按照文章中步骤操作即可；
+2、在main.js中另外添加↓
+import "../src/common/css/bootstrap.min.css";
--- src/components/nav-bar/nav-bar.vue
+++ src/components/nav-bar/nav-bar.vue
@@ -1,6 +1,19 @@
 <template>
-  <div id="nav-bar">
-    123
+  <div class="container">
+    <div id="navbar">
+      <a href="#">
+        <img class="dslogo" alt="Brand" src="../../common/images/ds-logo.png" />
+      </a>
+      <ul class="nav-link">
+        <li><a href="#">首页</a></li>
+        <li><a href="#">关于我们</a></li>
+        <li><a href="#">产品中心</a></li>
+        <li><a href="#">公司动态</a></li>
+        <li><a href="#">解决方案</a></li>
+        <li><a href="#">加入我们</a></li>
+        <li><a href="#">联系大善</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,12 +24,45 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-#nav-bar {
+#navbar{
+    display: flex;
+    justify-content: space-between;
     position: fixed;
     top: 0;
-    left: 0;
+    left: 50%;
+    transform: translateX(-50%)
     height: 80px;
-    width: 100%;
     background-color: #333;
+}
+@media (min-width: 768px) {
+    #navbar {
+        width: 750px;
+    }
+}
+
+//中等屏幕
+@media (min-width: 992px) {
+    #navbar {
+        width: 970px;
+    }
+}
+
+//大屏幕：1336px~更高
+@media (min-width: 1336px) {
+    #navbar {
+        width: 1336px;
+    }
+}
+.dslogo {
+    width: 200px;
+  }
+.nav-link {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+        margin-left: 20px;
+    }
 }
 </style>
